a9adf0238e6202e942434f8b1b0eb9bc21201dc8
Pirmin <pirmin.bleiker@thurweb.ch>
2021-09-28 10:54:55 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Das ist ein neuer Titel
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Whaaaaaat????
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
