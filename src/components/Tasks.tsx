ccc81e78c0cd79f39f7fdaf6d499adc271c22542
Pirmin <pirmin.bleiker@thurweb.ch>
2021-11-02 10:01:46 -0700

    testing 2

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -10,6 +10,6 @@ export const Tasks = ({tasks}: Props) => {
     const taskList = tasks.map((task) => (
          <SingleTask key={task.id}id={task.id}checked={task.checked}priority={task.priority}text={task.text}/>
     ));
-    <List data={taskList} />
+    return {taskList} 
 }
 
